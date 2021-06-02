cb3f4bd844df09a4831befeebacefe80b76f0274
Nathan Hui <nthui@eng.ucsd.edu>
2021-06-01 23:45:32 -0700

    Added automated CLI cycling

--- e4e/tests/smartfin_fw2_6.py
+++ e4e/tests/smartfin_fw2_6.py
@@ -7,9 +7,10 @@ class smartfin_fw2_6(e4e.test.Test):
         fin.reset()
 
     def test(self, fin:e4e.framework.Smartfin):
-        response = fin.sendCommand("#CLI", ">")
-        print(response)
-        IPython.embed()
+        for i in range(100):
+            fin.reset()
+            fin.sendCommand("#CLI", ">", 10)
+            assert(fin.getCurrentState() == e4e.framework.SMARTFIN_STATE.STATE_CLI)
 
     def cleanup(self, fin:e4e.framework.Smartfin):
         fin.reset()
