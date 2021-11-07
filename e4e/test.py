125f3a953ff9479b4948768c1416de793d93a8cf
Nathan Hui <nthui@eng.ucsd.edu>
2021-11-07 03:09:03 +0000

    Added plugin architecture for tests

--- e4e/test.py
+++ e4e/test.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
 import e4e.framework
+
+
 class Test(ABC):
-    def __init__(self, fin:e4e.framework.Smartfin):
+    def __init__(self, fin: e4e.framework.Smartfin) -> None:
         fin.reset()
 
     @abstractmethod
-    def test(self, fin:e4e.framework.Smartfin):
-        return
+    def test(self, fin: e4e.framework.Smartfin) -> None:
+        pass
 
-    def cleanup(self, fin:e4e.framework.Smartfin):
+    def cleanup(self, fin: e4e.framework.Smartfin) -> None:
         fin.reset()
--- testRunner.py
+++ testRunner.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import pkgutil
-from e4e.tests import *
 import e4e.test
 import os
 import importlib
@@ -8,8 +7,27 @@ import yaml
 import e4e.framework
 import traceback
 import datetime as dt
+from e4e import tests
+from typing import Type, Set
+import inspect
 
 TEST_RESULTS_DIR_FMT = "{name}_{timestamp}_{test}"
+
+def __all_subclasses(cls: Type[object]) -> Set[Type[object]]:
+    return set(cls.__subclasses__()).union(
+        [s for c in cls.__subclasses__() for s in __all_subclasses(c)]
+    )
+
+def iter_namespaces(ns_pkg):
+    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
+
+
+def load_plugins():
+    for _, name, _ in iter_namespaces(tests):
+        try:
+            importlib.import_module(name)
+        except:
+            print(f'Failed to import {name}')
 
 if __name__ == "__main__":
     with open(os.path.join('.', 'settings.yaml'), 'r') as stream:
@@ -19,10 +37,8 @@ if __name__ == "__main__":
 
     with open(os.path.join('.', 'e4e', 'tests', 'devices.yaml'), 'r') as stream:
         devices = yaml.safe_load(stream)
-    with open(os.path.join(".", "e4e", "tests", "tests.yaml"), 'r') as stream:
-        testClasses = yaml.safe_load(stream)
-    tests = {path.split('.')[-1]:path for path in testClasses}
-    testNames = [path.split('.')[-1] for path in testClasses]
+    load_plugins()
+    test_classes = list(__all_subclasses(e4e.test.Test))
     
 
 
@@ -39,37 +55,35 @@ if __name__ == "__main__":
 
         selectedDevice = sorted(devices.keys())[selectedDeviceIdx]
 
-        for i in range(len(tests)):
-            print("%4d: %s" % (i, testNames[i]))
+        for i in range(len(test_classes)):
+            print("%4d: %s" % (i, test_classes[i].__name__))
         print("%4d: %s" % (i + 1, "Exit"))
         
         selectedTestIdx = int(input())
         
-        assert(selectedTestIdx <= len(tests))
-        if selectedTestIdx == len(tests):
+        assert(selectedTestIdx <= len(test_classes))
+        if selectedTestIdx == len(test_classes):
             break
 
         try:
-            selectedTestModule = tests[testNames[selectedTestIdx]]
-            testPath = '.'.join(selectedTestModule.split('.')[:-1])
-            modulesource = importlib.import_module(testPath)
-            importlib.reload(modulesource)
+            selectedTestModule = test_classes[selectedTestIdx]
             testResultsDir = os.path.join(settings['testResults'], TEST_RESULTS_DIR_FMT.format(
                 name=selectedDevice,
-                test=testNames[selectedTestIdx],
+                test=test_classes[selectedTestIdx].__name__,
                 timestamp=dt.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
             ))
             os.mkdir(testResultsDir)
             with e4e.framework.Smartfin(port=devices[selectedDevice]['port'], 
                 results_dir=testResultsDir, name=selectedDevice, 
                 sfid=devices[selectedDevice]['sfid']) as fin:
-                testClass = getattr(modulesource, testNames[selectedTestIdx])
-                testInstance = testClass(fin)
+                assert(issubclass(selectedTestModule, e4e.test.Test))
+                assert(not inspect.isabstract(selectedTestModule))
+                testInstance: e4e.test.Test = selectedTestModule(fin)
                 try:
                     testInstance.test(fin)
-                    print("%s completed" % (selectedTestModule))
+                    print("%s completed" % (selectedTestModule.__name__))
                 except Exception as e:
-                    print("%s failed" % (selectedTestModule))
+                    print("%s failed" % (selectedTestModule.__name__))
                     raise e
                 finally:
                     testInstance.cleanup(fin)
