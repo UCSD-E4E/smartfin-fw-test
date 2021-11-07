866515b09c5e39ec00f5ac907534a21dbe9b441d
Nathan Hui <nthui@eng.ucsd.edu>
2021-11-07 01:27:16 +0000

    Updated base class to include reset

--- e4e/test.py
+++ e4e/test.py
@@ -1,14 +1,12 @@
 from abc import ABC, abstractmethod
 import e4e.framework
 class Test(ABC):
-    @abstractmethod
     def __init__(self, fin:e4e.framework.Smartfin):
+        fin.reset()
 
-        return
     @abstractmethod
     def test(self, fin:e4e.framework.Smartfin):
         return
 
-    @abstractmethod
     def cleanup(self, fin:e4e.framework.Smartfin):
-        return
+        fin.reset()
