acf6cf5870eea6148af9c99b846f16b8a8f127db
Nathan Hui <ntlhui@gmail.com>
2022-07-19 21:57:44 -0700

    Fixed requirements

--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
 from setuptools import setup, find_packages
 import e4e
+import os
+default_requires = [
+    'pandas',
+    'numpy',
+    'matplotlib',
+    'pyserial',
+    'google-api-python-client',
+    'google-auth-httplib2',
+    'google-auth-oauthlib',
+    'ipython',
+    'pyyaml',
+]
+pi_requires = [
+    'gpiozero',
+    'RPi.GPIO'
+]
 
+if os.path.exists('/sys/firmware/devicetree/base/model'):
+    with open('/sys/firmware/devicetree/base/model', 'r') as model:
+        pi_name = model.read()
+        if pi_name.find('Raspberry Pi') != -1:
+            default_requires.extend(pi_requires)
 setup(
     name='SmartfinFWTest',
     version=e4e.__VERSION__,
     author='UCSD Engineers for Exploration',
     author_email='e4e@eng.ucsd.edu',
-    install_requires=[
-        'pandas',
-        'numpy',
-        'matplotlib',
-        'gpiozero',
-        'pyserial',
-        'google-api-python-client',
-        'google-auth-httplib2',
-        'google-auth-oauthlib',
-        'ipython',
-        'pyyaml',
-        'RPi.GPIO'
-    ],
+    install_requires=default_requires,
     packages=find_packages()
 )
